faf91bce869ae522d68b70cdabdb98b815256d74
abugrin <zakariya.abugrin@gmail.com>
2020-05-20 19:54:43 +0200

    finished face_locations_onnx to iterate through files with report, show_images functionalities.

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -10,6 +10,10 @@ if __name__ == '__main__':
     # face_utilities.find_face_landmarks(image_path, enumerate_faces=True, report=True)
     # detect_imgs_onnx.find_face_locations(image_path, enumerate_faces=True, report=True)
 
+    images_folder = 'imgs'
+    face_utilities.face_locations_onnx(images_folder, lib='pil', report=True, show_images=True, save_images=True)
+
+'''
     label_path = "models/voc-model-labels.txt"
     result_path = "./detect_imgs_results_onnx"
     class_names = [name.strip() for name in open(label_path).readlines()]
@@ -23,5 +27,7 @@ if __name__ == '__main__':
         time_time = time.time()
         face_utilities.find_face_locations(image_path, face_detector='hog', report=True)
         print("cost time:{}".format(time.time() - time_time))
+'''
+
 
 
